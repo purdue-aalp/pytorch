c76908b3e918880c61df6870c6c79f7ebea9b26c
Christin David Bose <chris241@purdue.edu>
2025-03-20 18:40:17 -0400

    added scripts and changes to emb kernel optimizations

--- aten/src/ATen/native/cuda/EmbeddingBag.cu
+++ aten/src/ATen/native/cuda/EmbeddingBag.cu
@@ -165,7 +165,82 @@ __global__ void EmbeddingBag_updateOutputKernel_sum_mean(
     }
   }
 }
+/*
+// This kernel assumes that all input tensors except `weight` and
+// per_sample_weights are contiguous.
+template <typename scalar_t, typename index_t>
+__global__ void EmbeddingBag_updateOutputKernel_sum_mean(
+    const index_t *input, const index_t *offsets, const scalar_t *weight, scalar_t *output,
+    index_t *offset2bag, int64_t numIndices, int64_t numBags,
+    int64_t featureSize, int64_t weight_stride0, int64_t weight_stride1,
+    int mode, index_t *bag_size,
+    const scalar_t* per_sample_weights, int64_t per_sample_weights_stride,
+    index_t padding_idx, int64_t numRows) {
 
+  // the strategy here is that each bag x feature is handled by a single thread
+
+  using accscalar_t = acc_type<scalar_t, true>;
+  __shared__ scalar_t prefetch_data[256][10];
+  int tid =  blockDim.x * threadIdx.y + threadIdx.x;
+  int64_t chunksPerBag = ceil_div(featureSize, (int64_t)blockDim.x);
+  int64_t numChunks = numBags * chunksPerBag;
+  int64_t chunkOffset = blockIdx.x * blockDim.y + threadIdx.y;
+  int64_t chunkStride = gridDim.x * blockDim.y;
+
+  for (int64_t chunk = chunkOffset; chunk < numChunks; chunk += chunkStride) {
+    int64_t featureDim = (chunk % chunksPerBag) * blockDim.x + threadIdx.x;
+    if (featureDim < featureSize) {
+      int64_t bag = chunk / chunksPerBag;
+      const scalar_t *weightFeat = weight + featureDim * weight_stride1;
+      int64_t begin = bag == 0 ? 0 : offsets[bag]; // forces first offset to be 0 instead of asserting on it
+      int64_t end = (bag < numBags - 1) ? (offsets[bag + 1]) : numIndices;
+      CUDA_KERNEL_ASSERT(end >= begin);
+      accscalar_t weightFeatSum = 0;
+      int64_t bag_size_ = 0;
+      int ctr = 0;
+
+      for (int64_t emb = begin; emb < end; emb++) {
+        int ctr_mod;
+        ctr_mod = ctr%10;//assume pd is set to 4
+        if(ctr_mod==0){
+          int64_t weightRow;
+          #pragma unroll
+          for (int zz = 0; zz < 10; zz++){
+            weightRow = input[emb + zz] * weight_stride0;
+            prefetch_data[tid][zz] = weightFeat[weightRow];
+          }
+        }
+        bool pad = (input[emb] == padding_idx);
+        CUDA_KERNEL_ASSERT(input[emb] < numRows);
+        //const int64_t weightRow = input[emb] * weight_stride0;
+        //scalar_t weightValue = weightFeat[weightRow];
+        scalar_t weightValue = prefetch_data[tid][ctr_mod];
+        ctr++;
+        weightValue = pad ? static_cast<scalar_t>(0) : weightValue;
+        if (per_sample_weights) {
+          accscalar_t scaleWeightBy = static_cast<accscalar_t>(
+              per_sample_weights[emb * per_sample_weights_stride]);
+          weightFeatSum += scaleWeightBy * static_cast<accscalar_t>(weightValue);
+        } else {
+          weightFeatSum += static_cast<accscalar_t>(weightValue);
+        }
+        bag_size_ += pad ? 0 : 1;
+
+        if (featureDim == 0) {
+          offset2bag[emb] = bag;
+        }
+      }
+      if (mode == MODE_MEAN) {
+        if (bag_size_ != 0) {
+          weightFeatSum = weightFeatSum / static_cast<accscalar_t>(bag_size_);
+        }
+      }
+      bag_size[bag] = bag_size_;
+      output[bag * featureSize + featureDim] = static_cast<scalar_t>(weightFeatSum);
+    }
+  }
+}
+*/
 Tensor embedding_bag_backward_cuda_sum_avg(
                                    const Tensor &grad,
                                    const Tensor &indices_,
